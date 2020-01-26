ffd3e968c28490ac1ebf428411b285495a0eef86
Rita <rita@sandas.lt>
2020-01-26 23:09:15 +0200

    Changes

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -3,11 +3,97 @@
 # conda deactivate
 # python -m pytest
 # pytest -k  "debug"
+# /tests/files/Add.asm
+import sys
+import os
+
+PATH = "/tests/files/"
+FILE = "Add.asm"
+
+
+class Parser():
+
+    """
+    Consume assmebler command.
+    Returns command components dictionary. In addition, removes spaces and comments
+
+    Exp. @100 = {commandType: A_command, symbol: '100'}
+         M=1  = {'commandType': 'C_command', 'dest': 'M', 'comp': '1', 'jump': False}
+         @LOOP  = {commandType: L_command, symbol: 'LOOP'}
+    """
+    
+    def parse(command):
+        return {'commandType': 'C_command', 'dest': 'M', 'comp': '1', 'jump': False}
+
 
 class Assembler():
 
-     def main():
-         print("Hello world")
+    """ 
+    Return path to open and realitive path to given file
+
+    First check given file name in working directory, 
+    if not, check in  testing dir
+    else just get file example
+    """
+
+    def get_file_path(file_to_parse=False):
+        
+        # 1. Path in testing dir
+        path = os.getcwd() + PATH
+        file_to_open_t = path + FILE
+        file_to_open = ''
+
+        if file_to_parse:
+            file_to_open_t = os.getcwd() + PATH + file_to_parse
+            file_to_open_w = os.getcwd() + '/' + file_to_parse
+        
+        # if is file in working dir
+        if file_to_parse and os.path.exists(file_to_open_w):
+            file_to_open = file_to_open_w
+            path = os.getcwd() + '/'
+        # check if file is in testing dir    
+        elif os.path.exists(file_to_open_t):
+            file_to_open = file_to_open_t
+        return file_to_open, path
+
+    """ 
+    Path to Assembly .asm file to parse
+    Return binary file .hack
+    """
+
+    def main(file_to_parse=False):
+
+        if file_to_parse:
+            file_name_write = file_to_parse.split('.')[0] + ".hack"
+        else:
+            file_name_write = FILE.split('.')[0] + ".hack"
+
+        file_to_open = Assembler.get_file_path(file_to_parse)[0]
+        file_to_write = Assembler.get_file_path(file_to_parse)[1] + file_name_write
+        
+        # remove old file
+        if os.path.exists(file_to_write):
+            os.remove(file_to_write)
+            
+        try:
+            print("file to open", file_to_open, 'file_to_write', file_to_write)
+            with open(file_to_open) as f:
+                for line in f:
+                    parsed_command = Parser.parse(line)
+                    print(parsed_command)
+                    with open(file_to_write, 'a') as fw:
+                        fw.write(line)
+        except IOError:
+            print("No file")
+
 
 if __name__ == "__main__":
-    Assembler.main()
+    try:
+        try:
+            file_to_parse = sys.argv[1]
+            Assembler.main(file_to_parse)
+        except IndexError:
+            Assembler.main()
+    except TypeError:
+        print("Vista, ijunk python3")      
+    
--- assembler/tests/test_assembler.py
+++ assembler/tests/test_assembler.py
@@ -1,13 +1,81 @@
 import unittest
-from .. import assembler as amb
+import os
+from os import path
+from .. import assembler as ass
+
 
 class TestMyClass(unittest.TestCase):
 
-    def test_main(self):
+    def test_main_create_file(self):
 
-        result = amb.Assembler.main()
-        print("9", result)
-        self.assertFalse(result.span()[1] == 0)
+        ass.Assembler.main('A_instr.asm')
+        f = os.getcwd() + "/tests/files/A_instr.hack"
+        self.assertTrue(path.exists(f))
+
+        ass.Assembler.main('sample.asm')
+        f = os.getcwd() + "/sample.hack"
+        self.assertTrue(path.exists(f)) 
+
+    def test_parser_empty(self):
+        com = ass.Parser.parse('')
+        self.assertFalse(com)
+
+        com = ass.Parser.parse('   ')
+        self.assertFalse(com)
+
+        com = ass.Parser.parse('// ')
+        self.assertFalse(com)
+
+    def test_parser_A_command(self):
+        com = ass.Parser.parse('@100')
+        is_type = 'commandType' in com and 'A_command' == com['commandType']
+        is_symbol = 'symbol' in com and '100' == com['symbol']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_symbol)
+
+        com = ass.Parser.parse('@sum')
+        is_type = 'commandType' in com and 'A_command' == com['commandType']
+        is_symbol = 'symbol' in com and 'sum' == com['symbol']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_symbol)
+        
+        com = ass.Parser.parse('@i')
+        is_type = 'commandType' in com and 'A_command' == com['commandType']
+        is_symbol = 'symbol' in com and 'i' == com['symbol']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_symbol)
+
+    def test_parser_L_command(self):
+
+        com = ass.Parser.parse('@END')
+        is_type = 'commandType' in com and 'L_command' == com['commandType']
+        is_symbol = 'symbol' in com and 'END' == com['symbol']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_symbol)
+
+        com = ass.Parser.parse('@LOOP')
+        is_type = 'commandType' in com and 'L_command' == com['commandType']
+        is_symbol = 'symbol' in com and 'LOOP' == com['symbol']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_symbol)
+
+    def test_parser_C_command(self):
+
+        com = ass.Parser.parse('M=1')
+        is_type = 'commandType' in com and 'C_command' == com['commandType']
+        is_dest = 'dest' in com and 'M' == com['dest']
+        is_comp = 'comp' in com and '1' == com['comp']
+        is_jump = 'jump' in com and not com['jump']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_dest)
+        self.assertTrue(is_comp)
+        self.assertTrue(is_jump)
 
 
 if __name__ == '__main__':
