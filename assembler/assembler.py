024e5f9b452c067a58d712b8ec01186de2985afa
Rita <rita@sandas.lt>
2020-02-12 07:11:51 +0200

    Changes

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -10,6 +10,69 @@ import os
 PATH = "/tests/files/"
 FILE = "Add.asm"
 
+class Code():
+
+    """
+    Get parsed comp string
+    Returns the binary code 7 bits string of the comp
+
+    !!!
+    """
+
+    def get_comp(parsed_code):
+        return '0000000'
+
+
+    """
+    Get parsed jump string
+    Returns the binary code 3 bits string of the jump
+
+    !!!
+    """
+
+    def get_jump(parsed_code):
+        return '000'
+
+    """
+    Get parsed destination string
+    Returns the binary code 3 bits string of the dest
+
+    Exp. M - 001
+    !!!
+    """
+
+    def get_dest(parsed_code):
+        return '000'
+
+    """
+    Consume parsed dict
+    Return binary code
+
+    Exp. {'commandType': 'C_command', 'dest': 'M', 'comp': '1', 'jump': False}
+         1110 1111 1100 1000
+
+         {'commandType': 'C_command', 'dest': 'M', 'comp': '0', 'jump': False}
+         1110 10101 1000 1000
+
+         {'commandType': 'C_command', 'dest': False, 'comp': '0', 'jump': 'JMP'}
+         1110 1010 1000 0111
+
+
+    """
+    def get_code(parsed_dict):
+        code = ''
+        code_d = 'dest' in parsed_dict and Code.get_dest(parsed_dict['dest'])
+        code_c = 'comp' in parsed_dict and Code.get_comp(parsed_dict['comp'])
+        code_j = 'jump' in parsed_dict and Code.get_jump(parsed_dict['jump'])
+        if code_d:
+            code = code + code_d
+        if code_c:
+            code = code + code_c
+        if code_j:
+            code = code + code_j
+        print("dest", code_d, "comp", code_c, "jump", code_c, "binary code", code)
+        return code
+
 
 class Parser():
 
@@ -23,7 +86,34 @@ class Parser():
     """
     
     def parse(command):
-        return {'commandType': 'C_command', 'dest': 'M', 'comp': '1', 'jump': False}
+
+        parsed_command = {}
+        # Empty, spaces, comment
+        if not command or command[0] == ' ' or command[0] == '/' or command[0] == '\n':
+            return parsed_command
+        if command[0] == '@':
+            if command[1:].isupper():
+                parsed_command['commandType'] = 'L_command'
+            else:
+                parsed_command['commandType'] = 'A_command'
+        else:
+            parsed_command['commandType'] = 'C_command'  
+        if parsed_command['commandType'] == 'A_command' or parsed_command['commandType'] == 'L_command':
+            
+            symbol = command[1:].rstrip()
+            parsed_command['symbol'] = symbol
+        else:
+            if command[1] == '=':
+                parsed_command["dest"] = command[0]
+                parsed_command["comp"] = command[2:]
+                parsed_command["jump"] = False
+            else:
+                parsed_command["dest"] = False
+                parsed_command["comp"] = command[0]
+                parsed_command["jump"] = command[2:]
+
+        print("parsed_command", parsed_command)
+        return parsed_command
 
 
 class Assembler():
@@ -80,9 +170,9 @@ class Assembler():
             with open(file_to_open) as f:
                 for line in f:
                     parsed_command = Parser.parse(line)
-                    print(parsed_command)
+                    code = Code.get_code(parsed_command)
                     with open(file_to_write, 'a') as fw:
-                        fw.write(line)
+                        fw.write(code)
         except IOError:
             print("No file")
 
--- assembler/tests/test_assembler.py
+++ assembler/tests/test_assembler.py
@@ -70,13 +70,110 @@ class TestMyClass(unittest.TestCase):
         is_type = 'commandType' in com and 'C_command' == com['commandType']
         is_dest = 'dest' in com and 'M' == com['dest']
         is_comp = 'comp' in com and '1' == com['comp']
-        is_jump = 'jump' in com and not com['jump']
+        is_jump = 'jump' and com['jump'] == False
 
         self.assertTrue(is_type)
         self.assertTrue(is_dest)
         self.assertTrue(is_comp)
         self.assertTrue(is_jump)
 
+    def test_parser_C_command2(self):
+
+        com = ass.Parser.parse('D;JGT')
+        is_type = 'commandType' in com and 'C_command' == com['commandType']
+        is_dest = 'dest' and com['dest'] == False
+        is_comp = 'comp' in com and 'D' == com['comp']
+        is_jump = 'jump' in com and 'JGT' == com['jump']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_dest)
+        self.assertTrue(is_comp)
+        self.assertTrue(is_jump)
+
+    def test_parser_A_command3(self):
+
+        com = ass.Parser.parse('@i')
+
+        is_type = 'commandType' in com and 'A_command' == com['commandType']
+        is_symbol = 'symbol' in com and 'i' == com['symbol']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_symbol)
+
+    def test_parser_C_command4(self):
+
+        com = ass.Parser.parse('0;JMP')
+        is_type = 'commandType' in com and 'C_command' == com['commandType']
+        is_dest = 'dest' and com['dest'] == False
+        is_comp = 'comp' in com and '0' == com['comp']
+        is_jump = 'jump' in com and 'JMP' == com['jump']
+
+        self.assertTrue(is_type)
+        self.assertTrue(is_dest)
+        self.assertTrue(is_comp)
+        self.assertTrue(is_jump)
+
+    def test_code(self):
+
+        code = ass.Code.get_code({'commandType': 'C_command', 'dest': 'M', 'comp': '1', 'jump': False})
+        self.assertEqual('1110111111001000', code)
+
+        code = ass.Code.get_code({'commandType': 'C_command', 'dest': 'M', 'comp': '0', 'jump': False})
+        self.assertEqual('11101010110001000', code)
+
+        code = ass.Code.get_code({'commandType': 'C_command', 'dest': False, 'comp': '0', 'jump': 'JMP'})
+        self.assertEqual('1110101010000111', code)
+
+    def test_dest(self):
+
+        dest = ass.Code.get_dest(False)
+        self.assertEqual('000', dest)
+
+        dest = ass.Code.get_dest('M')
+        self.assertEqual('001', dest)
+
+        dest = ass.Code.get_dest('AM')
+        self.assertEqual('101', dest)
+
+    def test_jump(self):
+
+        jump = ass.Code.get_jump(False)
+        self.assertEqual('000', jump)
+
+        jump = ass.Code.get_jump('JGT')
+        self.assertEqual('001', jump)
+
+        jump = ass.Code.get_jump('JLE')
+        self.assertEqual('110', jump)
+
+    def test_comp(self):
+
+        comp = ass.Code.get_comp('0')
+        self.assertEqual('0101010', comp)
+
+        comp = ass.Code.get_comp('1')
+        self.assertEqual('0111111', comp)
+
+        comp = ass.Code.get_comp('D')
+        self.assertEqual('0001100', comp)
+
+        comp = ass.Code.get_comp('D+1')
+        self.assertEqual('0011111', comp)
+
+        comp = ass.Code.get_comp('M')
+        self.assertEqual('1110000', comp)
+
+        comp = ass.Code.get_comp('!M')
+        self.assertEqual('1110001', comp)
+
+        comp = ass.Code.get_comp('M+1')
+        self.assertEqual('1110111', comp)
+
+        comp = ass.Code.get_comp('D&M')
+        self.assertEqual('1000000', comp)
+
+        comp = ass.Code.get_comp('D&A')
+        self.assertEqual('0000000', comp)
 
 if __name__ == '__main__':
     unittest.main()
